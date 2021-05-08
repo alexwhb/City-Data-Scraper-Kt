f1df22895a20f21e46341705a85723312bd0b57e
Alex Black <alex.black@blackstoneaudio.com>
2021-05-08 12:42:24 -0700

    Updates build.gradle.kts with sqldelight and coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,27 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url ="https://www.jetbrains.com/intellij-repository/releases")
+        maven(url = "https://jetbrains.bintray.com/intellij-third-party-dependencies")
+    }
+
+    dependencies {
+        classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
+    }
+}
+
+
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.0"
+    id("com.squareup.sqldelight") version "1.4.4"
 }
 
 group = "com.blackstone"
 version = "1.0-SNAPSHOT"
+
 
 repositories {
     mavenCentral()
@@ -13,9 +29,18 @@ repositories {
 
 dependencies {
     implementation("it.skrape:skrapeit:1.1.1")
+    implementation( "com.squareup.sqldelight:sqlite-driver:1.4.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC")
+}
+
+sqldelight {
+   database("CityDataDB") {
+        packageName="com.citydata.db"
+        sourceFolders = listOf("sqldelight")
+   }
 }
 
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
