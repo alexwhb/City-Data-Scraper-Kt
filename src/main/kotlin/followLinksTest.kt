0ed3811a29303aa264221b38af5f48f60504a860
Alex Black <alex.black@blackstoneaudio.com>
2021-05-07 19:34:24 -0700

    Updates parser to extract much more data.

--- src/main/kotlin/followLinksTest.kt
+++ src/main/kotlin/followLinksTest.kt
@@ -2,8 +2,8 @@ import it.skrape.core.htmlDocument
 import it.skrape.fetcher.HttpFetcher
 import it.skrape.fetcher.extract
 import it.skrape.fetcher.skrape
+import it.skrape.selects.Doc
 import it.skrape.selects.html5.*
-import it.skrape.selects.text
 
 
 fun main() {
@@ -41,11 +41,46 @@ fun main() {
     extractCityData()
 }
 
+data class CityEcon(
+    val description: String,
+    val recentValue: String,
+    val yearOfRecentValue: String,
+    val oldValue: String,
+    val yearOfOldValue: String
+)
 
+data class RaceDemographic(val title: String, val percentageOfPopulation: Float, val totalPeople: Int)
 
-data class CityData(var medianAge: String? = null,
-                    var medianAgeForState: String? = null,
-                    val zipCodes : MutableList<String> = mutableListOf())
+data class MartelStatus(val title: String, val percentage: Float)
+
+data class EducationDemographic(val title: String, val percentage: Float)
+
+data class UnemploymentDemographic(val title:String, val percentage: Float)
+
+data class CityData(
+    var populationTotal: Int? = null,
+    var populationInYear: String? = null,
+    var malePopulation: Int? = null,
+    var femalePopulation: Int? = null,
+    var medianAge: Float? = null,
+    var medianAgeForState: Float? = null,
+    val zipCodes: MutableList<String> = mutableListOf(),
+    var medianHouseholdIncome: CityEcon? = null,
+    var medianIncomePerCapita: CityEcon? = null,
+    var medianHouseCondoValue: CityEcon? = null,
+    var costOfLivingIndex: String? = null,
+    var costOfLivingIndexDateDataAcquired: String? = null,
+    var landAreaOfCity: Measurement? = null,
+    var populationDensity: Measurement? = null,
+    var elevation: Measurement? = null,
+    var raceDemographic: MutableList<RaceDemographic> = mutableListOf(),
+    var maritalStatus: MutableList<MartelStatus> = mutableListOf(),
+    var educationDemographic: MutableList<EducationDemographic> = mutableListOf(),
+    var unemploymentDemographics: MutableList<UnemploymentDemographic> = mutableListOf()
+)
+
+data class Measurement(val amount: Float, val unitOfMeasure: String)
+
 
 // currently this just extracts data for one city, but it will easily be hooked up to our link extractor and
 // extract data for all cities. I do need to write some failsafe method so that if there's not a particular datapoint
@@ -58,65 +93,44 @@ fun extractCityData() {
         }
 
         extract {
-
             htmlDocument {
-                section {
-                    withClass = "median-age"
-                    tr {
-                        findFirst {
-                            cityData.medianAge = text
-                            print("$text\n")
+                extractCityPopulation(cityData)
+                extractMedianAge(cityData)
+                extractCityZipCodes(cityData)
+                extractIncomeDemographics(cityData)
+                extractCostOfLivingIndex(cityData)
+                extractRaceDemographics(cityData)
+                extractPopulationDensity(cityData)
+                extractCityElevation(cityData)
+                extractMaritalStatus(cityData)
+                extractEducationInfo(cityData)
+                extractUnemployment(cityData)
+            }
+        }
+    }
+        print(cityData)
+}
+
+
+private fun Doc.extractUnemployment(cityData: CityData) {
+    section {
+        withClass = "unemployment"
+        //*[@id="unemployment"]/div/table/tbody/tr[1]/td[2]/text()
+        findFirst {
+            tr {
+                findAll {
+                    forEach {
+                        var title = ""
+                        var percent = 0.0f
+                        it.td {
+                            findFirst {
+                                title = text
+                            }
+                            findSecond {
+                                percent = text.replace("%", "").toFloat()
+                            }
                         }
-                        findSecond {
-                            cityData.medianAgeForState = text
-                            print("$text\n")
-                        }
-                    }
-                }
-
-                section {
-                    withClass = "zip-codes"
-                    b {
-                        findAll {
-                            cityData.zipCodes.add(text)
-                        }
-                    }
-                }
-
-                section {
-                    withClass = "median-income"
-                    findFirst {
-                        val medianHomeIncome = parseEconValues(text, "Estimated median household income")
-                        val perCapitaIncome = parseEconValues(text, "Estimated per capita income")
-                        val meidanHouseCondoValue = parseEconValues(text, "Estimated median house or condo value")
-                        print(medianHomeIncome)
-                        print(perCapitaIncome)
-                        print(meidanHouseCondoValue)
-                    }
-                }
-
-                // cost of living info
-                section { withClass = "cost-of-living-index"
-                    findFirst {
-                        val groups = "(.+ \\d{4}) cost of living index in .+: ([0-9.]+)".toRegex().find(text)
-                        val timeDataWasGathered = groups?.groupValues?.get(1)
-                        val costOfLivingIndex = groups?.groupValues?.get(2)
-
-                        print("time data taken: $timeDataWasGathered. Cost of living index: $costOfLivingIndex \n")
-                    }
-                }
-
-                section {
-                    withClass = "population-density"
-                    findFirst {
-                        val groups = "Land area: ([0-9.]+) square (\\S+).".toRegex().find(text)
-                        val areaValue = groups?.groupValues?.get(1)
-                        val areaUnite = groups?.groupValues?.get(2)
-
-                        val populationDensityGroups = "Population density: (\\d+) people per square (\\S+)".toRegex().find(text)
-                        // todo these will be saved in a data class.
-                        val peoplePerUnit = groups?.groupValues?.get(1)
-                        val areaUnite2 = groups?.groupValues?.get(2)
+                        cityData.unemploymentDemographics.add(UnemploymentDemographic(title, percent))
                     }
                 }
             }
@@ -125,20 +139,216 @@ fun extractCityData() {
 }
 
 
-data class CityEcon(val description: String, val recentValue: String, val yearOfRecentValue: String, val oldValue: String, val yearOfOldValue: String)
+private fun Doc.extractEducationInfo(cityData: CityData) {
+    section {
+        withClass = "education-info"
+        ul {
+            li {
+                findAll {
+                    forEachIndexed { index, docElement ->
+                        if (index == this.size - 1) return@findAll
+                        val (status, percentage) = docElement.text.split(": ")
+                        cityData.educationDemographic.add(
+                            EducationDemographic(
+                                status,
+                                percentage.replace("%", "").toFloat()
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
 
-fun parseEconValues(text:String, beginsWith: String): CityEcon? {
-    val groups =  "$beginsWith in (\\d+): ([0-9,\\\$]+) \\(it was ([0-9,\\\$]+) in (\\d{4})\\)".toRegex().find(text)
+private fun Doc.extractMaritalStatus(cityData: CityData) {
+    section {
+        withClass = "marital-info"
+
+        ul {
+            li {
+                findAll {
+                    forEach {
+                        val (status, percentage) = it.text.split(": ")
+                        cityData.maritalStatus.add(MartelStatus(status, percentage.replace("%", "").toFloat()))
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun Doc.extractCityPopulation(cityData: CityData) {
+    section {
+        withClass = "city-population"
+        findFirst {
+            val groups = "Population in July (\\d{4}): ([0-9,]+).".toRegex().find(text)
+            if (groups != null) {
+                cityData.apply {
+                    populationTotal = groups.groupValues[2].replace(",", "").toInt()
+                    populationInYear = groups.groupValues[1]
+                }
+            }
+        }
+    }
+}
+
+private fun Doc.extractMedianAge(cityData: CityData) {
+    section {
+        withClass = "median-age"
+        tr {
+            findFirst {
+                val groups = "Median resident age: ([0-9.]+) years".toRegex().find(text)
+                if (groups != null) cityData.medianAge = groups.groupValues[1].toFloatOrNull()
+
+            }
+            findSecond {
+                val groups = " median age: ([0-9.]+) years".toRegex().find(text)
+                if (groups != null) cityData.medianAgeForState = groups.groupValues[1].toFloatOrNull()
+
+            }
+        }
+    }
+}
+
+private fun Doc.extractCityZipCodes(cityData: CityData) {
+    section {
+        withClass = "zip-codes"
+        a {
+            findAll {
+                this.forEach {
+                    cityData.zipCodes.add(it.text)
+                }
+            }
+        }
+    }
+}
+
+private fun Doc.extractIncomeDemographics(cityData: CityData) {
+    section {
+        withClass = "median-income"
+        findFirst {
+            cityData.apply {
+                medianHouseholdIncome = parseEconValues(text, "Estimated median household income")
+                medianIncomePerCapita = parseEconValues(text, "Estimated per capita income")
+                medianHouseCondoValue = parseEconValues(text, "Estimated median house or condo value")
+            }
+        }
+    }
+}
+
+private fun Doc.extractCostOfLivingIndex(cityData: CityData) {
+    section {
+        withClass = "cost-of-living-index"
+        findFirst {
+            val groups = "(.+ \\d{4}) cost of living index in .+: ([0-9.]+)".toRegex().find(text)
+            cityData.apply {
+                costOfLivingIndex = groups?.groupValues?.get(2)
+                costOfLivingIndexDateDataAcquired = groups?.groupValues?.get(1)
+            }
+        }
+    }
+}
+
+private fun Doc.extractPopulationDensity(cityData: CityData) {
+    section {
+        withClass = "population-density"
+        findFirst {
+            cityData.apply {
+                val groups = "Land area: ([0-9.]+) (\\S+).".toRegex().find(text)
+                if (groups != null && groups.groupValues.size >= 3) {
+                    landAreaOfCity = Measurement(groups.groupValues[1].toFloat(), groups.groupValues[2])
+                }
+            }
+
+            cityData.apply {
+                val groups = "Population density: (\\d+) people per (\\S+)".toRegex().find(text)
+                if (groups != null && groups.groupValues.size >= 3) {
+                    populationDensity = Measurement(groups.groupValues[1].toFloat(), groups.groupValues[2])
+                }
+            }
+        }
+    }
+}
+
+private fun Doc.extractCityElevation(cityData: CityData) = section {
+    withClass = "elevation"
+
+    findFirst {
+        val groups = "Elevation: (\\d+) (\\S+)".toRegex().find(text)
+        cityData.apply {
+            if (groups != null && groups.groupValues.size >= 3) {
+                elevation = Measurement(groups.groupValues[1].toFloat(), groups.groupValues[2])
+            }
+        }
+    }
+}
+
+private fun Doc.extractRaceDemographics(cityData: CityData) {
+    section {
+        withClass = "races-graph"
+        ul {
+            withClass = "list-group"
+            findSecond {
+                li {
+                    findAll {
+                        forEach {
+                            var title = ""
+                            var numberOfPeople = 0
+                            var percentageOfPopulation = 0.0f
+                            it.b {
+                                findFirst {
+                                    title = text
+                                }
+                                it.span {
+                                    withClass = "alert-info"
+                                    findFirst {
+                                        percentageOfPopulation = text.replace("%", "").toFloat()
+                                    }
+                                }
+                                it.span {
+                                    withClass = "badge"
+                                    findFirst {
+                                        numberOfPeople = text.replace(",", "").toInt()
+                                    }
+                                }
+                            }
+                            cityData.raceDemographic.add(
+                                RaceDemographic(
+                                    title,
+                                    percentageOfPopulation,
+                                    numberOfPeople
+                                )
+                            )
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+fun parseEconValues(text: String, beginsWith: String): CityEcon? {
+    val groups = "$beginsWith in (\\d+): ([0-9,\\\$]+) \\(it was ([0-9,\\\$]+) in (\\d{4})\\)".toRegex().find(text)
 
     if (groups?.groupValues?.size == 5) {
-        return CityEcon(beginsWith, groups.groupValues[2], groups.groupValues[1],groups.groupValues[3], groups.groupValues[4])
+        return CityEcon(
+            beginsWith,
+            groups.groupValues[2],
+            groups.groupValues[1],
+            groups.groupValues[3],
+            groups.groupValues[4]
+        )
     }
     return null
 }
 
 
 fun createLink(cityName: String, stateName: String): String {
-   return "https://www.city-data.com/city/$cityName-$stateName.html"
+    return "https://www.city-data.com/city/$cityName-$stateName.html"
 }
 
 fun extractCityDataFromState(stateName: String, stateUrl: String) {
@@ -152,37 +362,46 @@ fun extractCityDataFromState(stateName: String, stateUrl: String) {
                 table {
                     withId = "cityTAB"
                     tr {
-                       this.findAll {
-                           mapIndexed { index, docElement ->
-                               // skip the first row because this just contains the headings
-                               if(index == 0) return@mapIndexed
-                               docElement.td {
-                                   findAll {
-                                       mapIndexed { index, docElement ->
-                                           // skip the first col because this just contains map info
-                                           if(index != 0) {
-                                               // function l(url) { window.location = url + "-" + stateName;}
-                                               if(index == 1) {
-                                                   print("city name: ${docElement.text}\n")
+                        this.findAll {
+                            mapIndexed { index, docElement ->
+                                // skip the first row because this just contains the headings
+                                if (index == 0) return@mapIndexed
+                                docElement.td {
+                                    findAll {
+                                        mapIndexed { index, docElement ->
+                                            // skip the first col because this just contains map info
+                                            if (index != 0) {
+                                                // function l(url) { window.location = url + "-" + stateName;}
+                                                if (index == 1) {
+                                                    print("city name: ${docElement.text}\n")
 
-                                                   // if link ends with .html don't do this
-                                                   // docElement.eachHref.first().replace("javascript:l(\"", "").replace("\");", "")
-                                                   if(docElement.eachHref.first().endsWith(".html")) {
-                                                       print("link : ${docElement.eachHref.first()}\n")
-                                                   } else {
-                                                       print("link : ${createLink(docElement.eachHref.first().replace("javascript:l(\"", "").replace("\");", ""), stateName)}\n")
-                                                   }
+                                                    // if link ends with .html don't do this
+                                                    // docElement.eachHref.first().replace("javascript:l(\"", "").replace("\");", "")
+                                                    if (docElement.eachHref.first().endsWith(".html")) {
+                                                        print("link : ${docElement.eachHref.first()}\n")
+                                                    } else {
+                                                        print(
+                                                            "link : ${
+                                                                createLink(
+                                                                    docElement.eachHref.first()
+                                                                        .replace("javascript:l(\"", "")
+                                                                        .replace("\");", ""),
+                                                                    stateName
+                                                                )
+                                                            }\n"
+                                                        )
+                                                    }
 
 
-                                               } else if(index == 2) {
-                                                   print("city population: ${docElement.text}\n")
-                                               }
-                                           }
-                                       }
-                                   }
-                               }
-                           }
-                       }
+                                                } else if (index == 2) {
+                                                    print("city population: ${docElement.text}\n")
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
